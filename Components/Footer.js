cddeb1ebaeb6ff9e97d723af2cb94a796e42e4e1
siddharth tutorial <ggvanecha@gmail.com>
2025-03-13 19:08:50 +0530

    components

---
+++ Components/Footer.js
@@ -0,0 +1,16 @@
+// import { Padding } from '@mui/icons-material'
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <Box style={{Padding:"20px", backgroundColor: '#333', color: '#fff' }}>
+        <Typography variant='body2' align='center'>
+        © 2025 My Blog. All rights reserved.
+        </Typography>
+      
+    </Box>
+  )
+}
+
+export default Footer
